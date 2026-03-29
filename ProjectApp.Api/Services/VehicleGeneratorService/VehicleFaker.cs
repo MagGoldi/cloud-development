bc7294b7ddaca0616e473a4a0ecef5201831afb7
MagGoldi <stivenH0king@yandex.ru>
2026-03-29 16:03:10 +0400

    corrections 2

--- ProjectApp.Api/Services/VehicleGeneratorService/VehicleFaker.cs
+++ ProjectApp.Api/Services/VehicleGeneratorService/VehicleFaker.cs
@@ -5,52 +5,52 @@ namespace ProjectApp.Api.Services.VehicleGeneratorService;
 
 public class VehicleFaker
 {
-    private static readonly string[] FuelTypes = ["Бензин", "Дизель", "Электро", "Гибрид", "Газ"];
+    private static readonly string[] _fuelTypes = ["Бензин", "Дизель", "Электро", "Гибрид", "Газ"];
 
-    private static readonly Dictionary<string, string[]> BrandModels = new()
+    private static readonly string[] _bodyTypes = ["Седан", "Хэтчбек", "Универсал", "Кроссовер", "Внедорожник", "Купе", "Минивэн", "Пикап"];
+
+    private static readonly Dictionary<string, string[]> _brandModels = new()
     {
-        ["Toyota"]      = ["Camry", "Corolla", "RAV4", "Land Cruiser", "Yaris", "Highlander", "Prado"],
-        ["BMW"]         = ["3 Series", "5 Series", "7 Series", "X3", "X5", "X6", "M4"],
-        ["Mercedes"]    = ["C-Class", "E-Class", "S-Class", "GLE", "GLC", "A-Class", "CLA"],
-        ["Volkswagen"]  = ["Passat", "Golf", "Tiguan", "Polo", "Touareg", "Jetta", "ID.4"],
-        ["Hyundai"]     = ["Solaris", "Tucson", "Santa Fe", "Creta", "Elantra", "Sonata", "i30"],
-        ["Ford"]        = ["Focus", "Mondeo", "Explorer", "Kuga", "Puma", "Mustang", "Ranger"],
-        ["Kia"]         = ["Rio", "Sportage", "Sorento", "Ceed", "K5", "Seltos", "Stinger"],
-        ["Audi"]        = ["A3", "A4", "A6", "Q3", "Q5", "Q7", "TT"],
-        ["Nissan"]      = ["Qashqai", "X-Trail", "Altima", "Leaf", "Juke", "Murano", "Note"],
-        ["Renault"]     = ["Logan", "Duster", "Sandero", "Megane", "Arkana", "Captur", "Laguna"],
-        ["Lada"]        = ["Granta", "Vesta", "Largus", "Niva Travel", "XRAY", "4x4"],
-        ["Skoda"]       = ["Octavia", "Superb", "Kodiaq", "Karoq", "Fabia", "Scala", "Kamiq"],
-        ["Mazda"]       = ["Mazda3", "Mazda6", "CX-5", "CX-9", "MX-5", "CX-30"],
-        ["Honda"]       = ["Civic", "Accord", "CR-V", "HR-V", "Jazz", "Pilot"],
-        ["Subaru"]      = ["Outback", "Forester", "Impreza", "XV", "Legacy", "WRX"],
+        ["Toyota"]     = ["Camry", "Corolla", "RAV4", "Land Cruiser", "Yaris", "Highlander", "Prado"],
+        ["BMW"]        = ["3 Series", "5 Series", "7 Series", "X3", "X5", "X6", "M4"],
+        ["Mercedes"]   = ["C-Class", "E-Class", "S-Class", "GLE", "GLC", "A-Class", "CLA"],
+        ["Volkswagen"] = ["Passat", "Golf", "Tiguan", "Polo", "Touareg", "Jetta", "ID.4"],
+        ["Hyundai"]    = ["Solaris", "Tucson", "Santa Fe", "Creta", "Elantra", "Sonata", "i30"],
+        ["Ford"]       = ["Focus", "Mondeo", "Explorer", "Kuga", "Puma", "Mustang", "Ranger"],
+        ["Kia"]        = ["Rio", "Sportage", "Sorento", "Ceed", "K5", "Seltos", "Stinger"],
+        ["Audi"]       = ["A3", "A4", "A6", "Q3", "Q5", "Q7", "TT"],
+        ["Nissan"]     = ["Qashqai", "X-Trail", "Altima", "Leaf", "Juke", "Murano", "Note"],
+        ["Renault"]    = ["Logan", "Duster", "Sandero", "Megane", "Arkana", "Captur", "Laguna"],
+        ["Lada"]       = ["Granta", "Vesta", "Largus", "Niva Travel", "XRAY", "4x4"],
+        ["Skoda"]      = ["Octavia", "Superb", "Kodiaq", "Karoq", "Fabia", "Scala", "Kamiq"],
+        ["Mazda"]      = ["Mazda3", "Mazda6", "CX-5", "CX-9", "MX-5", "CX-30"],
+        ["Honda"]      = ["Civic", "Accord", "CR-V", "HR-V", "Jazz", "Pilot"],
+        ["Subaru"]     = ["Outback", "Forester", "Impreza", "XV", "Legacy", "WRX"],
     };
-
-    private const string RegistrationLetters = "АВЕКМНОРСТУХ";
 
     private readonly Faker<Vehicle> _faker;
 
     public VehicleFaker()
     {
+        var currentYear = DateTime.Now.Year;
+
         _faker = new Faker<Vehicle>("ru")
             .RuleFor(v => v.Id, f => f.IndexFaker + 1)
-            .RuleFor(v => v.Brand, f => f.PickRandom(BrandModels.Keys.ToArray()))
-            .RuleFor(v => v.Model, (f, v) => f.PickRandom(BrandModels[v.Brand]))
-            .RuleFor(v => v.RegistrationNumber, f =>
+            .RuleFor(v => v.Vin, f => f.Vehicle.Vin())
+            .RuleFor(v => v.Brand, f => f.PickRandom(_brandModels.Keys.ToArray()))
+            .RuleFor(v => v.Model, (f, v) => f.PickRandom(_brandModels[v.Brand]))
+            .RuleFor(v => v.Year, f => f.Random.Int(1984, currentYear))
+            .RuleFor(v => v.BodyType, f => f.PickRandom(_bodyTypes))
+            .RuleFor(v => v.FuelType, f => f.PickRandom(_fuelTypes))
+            .RuleFor(v => v.Color, f => f.Commerce.Color())
+            .RuleFor(v => v.Mileage, (f, v) => Math.Round(f.Random.Double(0, 500_000), 1))
+            .RuleFor(v => v.LastServiceDate, (f, v) =>
             {
-                var l1 = RegistrationLetters[f.Random.Int(0, RegistrationLetters.Length - 1)];
-                var digits = f.Random.Int(100, 999);
-                var l2 = RegistrationLetters[f.Random.Int(0, RegistrationLetters.Length - 1)];
-                var l3 = RegistrationLetters[f.Random.Int(0, RegistrationLetters.Length - 1)];
-                var region = f.Random.Int(10, 199);
-                return $"{l1}{digits}{l2}{l3} {region}";
-            })
-            .RuleFor(v => v.OwnerName, f => f.Name.FullName())
-            .RuleFor(v => v.Year, f => f.Random.Int(1984, 2026))
-            .RuleFor(v => v.EngineVolume, f => Math.Round(f.Random.Decimal(0.8m, 6.0m), 1))
-            .RuleFor(v => v.Mileage, f => f.Random.Int(0, 500_000))
-            .RuleFor(v => v.FuelType, f => f.PickRandom(FuelTypes))
-            .RuleFor(v => v.Price, f => Math.Round(f.Random.Decimal(100_000m, 10_000_000m), 0));
+                var minDate = new DateOnly(v.Year, 1, 1);
+                var maxDate = DateOnly.FromDateTime(DateTime.Now);
+                var range = maxDate.DayNumber - minDate.DayNumber;
+                return minDate.AddDays(f.Random.Int(0, range));
+            });
     }
 
     public Vehicle Generate() => _faker.Generate();
--- ProjectApp.Domain/Entities/Vehicle.cs
+++ ProjectApp.Domain/Entities/Vehicle.cs
@@ -8,30 +8,30 @@ public class Vehicle
     /// <summary>Уникальный идентификатор транспортного средства в системе</summary>
     public required int Id { get; set; }
 
-    /// <summary>Марка (производитель) транспортного средства</summary>
+    /// <summary>VIN-номер транспортного средства</summary>
+    public required string Vin { get; set; }
+
+    /// <summary>Производитель транспортного средства</summary>
     public required string Brand { get; set; }
 
     /// <summary>Модель транспортного средства</summary>
     public required string Model { get; set; }
 
-    /// <summary>Государственный регистрационный номер</summary>
-    public required string RegistrationNumber { get; set; }
-
-    /// <summary>Полное имя владельца транспортного средства</summary>
-    public required string OwnerName { get; set; }
-
     /// <summary>Год выпуска транспортного средства</summary>
     public int Year { get; set; }
 
-    /// <summary>Объём двигателя в литрах</summary>
-    public decimal EngineVolume { get; set; }
-
-    /// <summary>Пробег в километрах</summary>
-    public int Mileage { get; set; }
+    /// <summary>Тип корпуса (кузова)</summary>
+    public required string BodyType { get; set; }
 
     /// <summary>Тип используемого топлива</summary>
     public required string FuelType { get; set; }
 
-    /// <summary>Рыночная стоимость транспортного средства в рублях</summary>
-    public decimal Price { get; set; }
+    /// <summary>Цвет корпуса</summary>
+    public required string Color { get; set; }
+
+    /// <summary>Пробег в километрах</summary>
+    public double Mileage { get; set; }
+
+    /// <summary>Дата последнего техобслуживания</summary>
+    public DateOnly LastServiceDate { get; set; }
 }
